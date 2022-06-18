16b78f20d08322bd4f3711b524e9167795de5bf6
yash bhaskar <92648818+yash9439@users.noreply.github.com>
2022-06-19 00:39:22 +0530

    Update Mul_1_Add_zero.py

--- Mul_1_Add_zero.py
+++ Mul_1_Add_zero.py
@@ -20,18 +20,10 @@ for i in range(len(Lines)):
 Lines.insert(0,"#define one 1")
 Lines.insert(0,"#define zero 0")
 
-# for i in range(len(Lines)):
-#     for j in range(len(Lines[i])-1):
-#         if Lines[i][j].isdigit():
-#             if j == 0:
-#                 continue
-#             if (Lines[i][j-1] == " " or Lines[i][j-1] == "-") and (Lines[i][j+1] == " " or Lines[i][j+1] == ";" or Lines[i][j+1] == ","):
-#                 if(j-2 >= 0):
-#                     if(Lines[i][j-2] != " "):
-#                         continue
-#                 Lines[i] = Lines[i][:j+1] + "*one+zero" + Lines[i][j+1:]
 
 
+
+# adding *one+zero to alone integers
 for i in range(len(Lines)):
     if("define" in Lines[i]):
         continue
@@ -45,35 +37,9 @@ for i in range(len(Lines)):
     Lines[i] = tempLine
 
 
-# for i in range(len(Lines)):
-#     if("define" in Lines[i]):
-#         continue
-#     temp = Lines[i].split()
-#     for j in range(len(temp)):
-#         if(temp[j][:len(temp[j])-2].isnumeric() and temp[j][len(temp[j]-1)] == ";"):
-#             temp[j] = temp[:len(temp[j])-2] + "*one+zero;"
-#     tempLine = ""
-#     for j in temp:
-#         tempLine = tempLine + j + " "
-#     Lines[i] = tempLine
 
 
-for i in range(len(Lines)):
-    if("define" in Lines[i]):
-        continue
-    temp = Lines[i].split()
-    tempLine = ""
-    for j in range(len(temp)):
-        if(temp[j][len(temp[j])-1] == ';' and (temp[j][:(len(temp[j])-1)]).strip().isnumeric()):
-            tempLine = tempLine + temp[j][:(len(temp[j])-1)] + "*one+zero;" + " "
-        else:
-            tempLine = tempLine + temp[j] + " "
-    # for j in temp:
-        # tempLine = tempLine + j + " "
-    Lines[i] = tempLine
-
-
-
+# Adding that to 9; , etc cases
 for i in range(len(Lines)):
     if("define" in Lines[i]):
         continue
@@ -87,9 +53,26 @@ for i in range(len(Lines)):
         tempLine = tempLine + Lines[i][j]
     Lines[i] = tempLine
 
-    # for j in temp:
-        # tempLine = tempLine + j + " "
+
+
+
+# [200] cases
+for i in range(len(Lines)):
+    if("define" in Lines[i]):
+        continue
+    tempLine = ""
+    for j in range(len(Lines[i])):
+        if(Lines[i][j].isnumeric()):
+            if(j-1 >= 0 and j+1 < len(Lines[i])):
+                if((Lines[i][j-1] == '+' or Lines[i][j-1] == '-' or Lines[i][j-1] == ' ' or Lines[i][j-1] == '[') and (Lines[i][j+1] == ']' or Lines[i][j+1] == ';' or Lines[i][j+1] == ' ')):
+                    tempLine = tempLine + Lines[i][j] + "*one+zero"
+                    continue
+        tempLine = tempLine + Lines[i][j]
     Lines[i] = tempLine
+
+
+
+
 
 for i in range(len(Lines)):
 	Lines[i] = Lines[i] + "\n"
