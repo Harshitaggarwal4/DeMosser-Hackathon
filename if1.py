44d6733a82c338e8039d8c4ac4b67fc1bcd7d255
yash bhaskar <92648818+yash9439@users.noreply.github.com>
2022-06-19 14:34:06 +0530

    if1 Code
    adding if(1) {}

---
+++ if1.py
@@ -0,0 +1,49 @@
+import os
+os.system('clang-format -style=Google -i ./input2.c')
+
+
+with open('input.c','r') as f:
+	Lines = f.readlines()
+
+
+
+for i in range(len(Lines)):
+	Lines[i] = Lines[i].strip()
+
+
+dataTypes = ["char", "float", "double","int", "long", "long int", "long long", "long long int"]
+
+chk = 0
+for i in range(len(Lines)):
+    if("int main" in Lines[i]):
+        chk = 1
+        continue
+    if("int main" in Lines[i] or "for" in Lines[i] or "for" in Lines[i] or "for" in Lines[i] and chk == 1):
+        continue
+    if("scanf" in Lines[i]):
+        tempStr = ""
+        tempStr = tempStr +  "if(1) { " + Lines[i] + "} else { printf(\"Error\");}"
+        Lines[i] = tempStr
+        continue
+    if("printf" in Lines[i]):
+        tempStr = ""
+        tempStr = tempStr +  "if(1) { " + Lines[i] + "} else { printf(\"Error\");}"
+        Lines[i] = tempStr
+        continue
+    token = Lines[i].split(" ")
+    for j in token:
+        if j in dataTypes:
+            tempStr = ""
+            tempStr = tempStr +  "if(1) { " + Lines[i] + "} else { printf(\"Error\");}" + Lines[i]
+            Lines[i] = tempStr
+            break
+
+
+
+for i in range(len(Lines)):
+	Lines[i] = Lines[i] + "\n"
+
+file1 = open('out.c', 'w')
+file1.writelines(Lines)
+file1.close()
+os.system('clang-format -style=Google -i ./out.c')
