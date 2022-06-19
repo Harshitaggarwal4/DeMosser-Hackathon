98859d97b203a90ea57ed46ff5bf8ee80ff5da4e
Sreyas S <futuresreyas@gmail.com>
2022-06-19 05:53:12 +0530

    Deals with i++ and ++i properly
    Correcting my dumb mistake of not handling ++i and i++ differently

--- changeIncrement.py
+++ changeIncrement.py
@@ -1,4 +1,4 @@
-input_c = "input.c"
+input_c = "test.c"
 output_c = "out2.c"
 
 in1 = open(input_c, "r")
@@ -9,7 +9,56 @@ lines = in1.readlines()
 length = len(lines)
 for line_Ind in range(length):
     line = lines[line_Ind]
-    line = line.replace("++", "+=1")
+    inc_Ind = line.find("++")
+    if inc_Ind == -1:
+        continue
+    newLine = line.split("++")
+    # print(newLine)
+    if newLine[1][0].isalpha():
+        newLine[1] = newLine[1].replace(","," ")
+        newLine[1] = newLine[1].replace("["," ")
+        newLine[1] = newLine[1].replace("("," ")
+        newLine[1] = newLine[1].replace("]"," ")
+        newLine[1] = newLine[1].replace(")"," ")
+        newLine[1] = newLine[1].replace("{"," ")
+        newLine[1] = newLine[1].replace("}"," ")
+        newLine[1] = newLine[1].replace("\""," ")
+        newLine[1] = newLine[1].replace("'"," ")
+        newLine[1] = newLine[1].replace("="," ")
+        newLine[1] = newLine[1].replace("+"," ")
+        newLine[1] = newLine[1].replace("-"," ")
+        newLine[1] = newLine[1].replace("*"," ")
+        newLine[1] = newLine[1].replace("/"," ")
+        newLine[1] = newLine[1].replace(";"," ")
+
+        newSub = newLine[1].split()
+        name = newSub[0]
+        line = line.replace("++%s"%name, "%s+=1"%name)
+
+    else:
+
+        newLine[0] = newLine[0].replace("["," ")
+        newLine[0] = newLine[0].replace("("," ")
+        newLine[0] = newLine[0].replace("]"," ")
+        newLine[0] = newLine[0].replace(")"," ")
+        newLine[0] = newLine[0].replace("{"," ")
+        newLine[0] = newLine[0].replace("}"," ")
+        newLine[0] = newLine[0].replace("\""," ")
+        newLine[0] = newLine[0].replace("'"," ")
+        newLine[0] = newLine[0].replace(","," ")
+        newLine[0] = newLine[0].replace("="," ")
+        newLine[0] = newLine[0].replace("+"," ")
+        newLine[0] = newLine[0].replace("-"," ")
+        newLine[0] = newLine[0].replace("*"," ")
+        newLine[0] = newLine[0].replace("/"," ")
+        newLine[0] = newLine[0].replace(";"," ")
+
+        print(newLine[0])
+        newSub = newLine[0].split()
+        name = newSub[-1]
+        line = line.replace("++", "+=0")
+        lines.insert(line_Ind+1, "%s+=1;\n"%name)
+
     lines[line_Ind] = line
 
 for line in lines:
