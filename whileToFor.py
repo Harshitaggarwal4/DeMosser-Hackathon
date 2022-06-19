cc3c4644bc8f9511d482dc0c9297de50a10ef862
yash bhaskar <92648818+yash9439@users.noreply.github.com>
2022-06-19 08:43:24 +0530

    WhileToFor
    Changing the while loops into for loops

---
+++ whileToFor.py
@@ -0,0 +1,32 @@
+import os
+from tracemalloc import start
+os.system('clang-format -style=Google -i ./input2.c')
+
+
+with open('input2.c','r') as f:
+	Lines = f.readlines()
+
+
+
+for i in range(len(Lines)):
+	Lines[i] = Lines[i].strip()
+
+
+import re
+for i in range(len(Lines)):
+    if("while" in Lines[i]):
+        condition = re.search('\((.*)\)', Lines[i])
+        Lines[i] = "for(;" + condition.group(1) + ";) {"
+
+
+
+
+
+for i in range(len(Lines)):
+	Lines[i] = Lines[i] + "\n"
+
+file1 = open('out.c', 'w')
+file1.writelines(Lines)
+os.system('clang-format -style=Google -i ./out.c')
+file1.close()
+
